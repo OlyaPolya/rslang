9db3badcb897999916e084823b5f599dcc68c2df
Olya_Polyanskaya <polyanskaya3000@gmail.com>
2022-08-23 14:41:01 +0700

    fix: make className and innerText as optional

--- src/core/components/UIComponent.ts
+++ src/core/components/UIComponent.ts
@@ -1,16 +1,20 @@
 interface Component {
   parentTagName: string;
   tagName: string;
-  className: string;
-  innerText: string;
+  className?: string;
+  innerText?: string;
 }
 
 export default function createUIComponent(component: Component) {
   const parentContainer = document.createElement(`${component.parentTagName}`);
   const container = document.createElement(`${component.tagName}`);
 
-  container.className = component.className;
-  container.innerText = component.innerText;
+  if (component.className) {
+    container.className = component.className;
+  } 
+  if (component.innerText) {
+    container.innerText = component.innerText;
+  } 
   parentContainer.appendChild(container);
 
   return parentContainer;
